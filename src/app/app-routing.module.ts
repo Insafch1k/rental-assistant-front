004c663f08892b6af29b87b144c008fb31a86622
ZakiCDP <Zahar100403@mail.ru>
2025-05-25 22:01:38 +0300

    Added lazeload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 
 const routes: Routes = [
-  { path: '', component: MainComponent, pathMatch: 'full' },
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/main/main.module').then((p) => p.MainModule),
+    pathMatch: 'full',
+  },
+  {
+    path: 'profile/edit/:id',
+    loadChildren: () =>
+      import('./pages/edit/edit.module').then((p) => p.EditModule),
+  },
+  {
+    path: 'profile/:id',
+    loadChildren: () =>
+      import('./pages/profile/profile.module').then((p) => p.ProfileModule),
+  },
 ];
 
 @NgModule({
